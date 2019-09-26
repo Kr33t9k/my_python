5f0d69263aed29a2428a2b276ee676bd83f7138f
Рахман <55310365+Kr33t9k@users.noreply.github.com>
2019-09-26 11:45:35 +0300

    6.3 Удалить из списка все элементы кроме выбранных
    Исправил

--- del_element_list.py
+++ del_element_list.py
@@ -1,6 +1,6 @@
 names = ['John', 'Paul', 'George', 'Ringo']
-for i in names:
-    if i != 'John' or i != 'Paul':
+for i in reversed(names):
+    if i != 'John' and i != 'Paul':
         names.remove(i)
 print(names)
 
