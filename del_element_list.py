71e3f4ba333a02bbc737ea55ed1e82cb20b3aea5
Рахман <55310365+Kr33t9k@users.noreply.github.com>
2019-09-26 12:02:12 +0300

    6.3 Удалить из списка все элементы кроме выбранных
    Исправил

--- del_element_list.py
+++ del_element_list.py
@@ -1,8 +1,5 @@
-names = ['John', 'Paul', 'George', 'Ringo']
-for i in reversed(names):
-    if i != 'John' and i != 'Paul':
-        names.remove(i)
-print(names)
+names = ['Rakhman', 'John', 'Paul', 'George', 'Ringo']
+print(list(filter((lambda x: x == 'Paul' or x == 'John'), names)))
 
 
 '''Отфильтровать список имен так, чтобы в нем остался только элемент 'John' или 'Paul'.
