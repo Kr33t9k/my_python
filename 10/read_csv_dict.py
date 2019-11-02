28fcabafda4a3facba6067c62436d6378f411a68
kr33t9k <Kr33t9k@users.noreply.github.com>
2019-11-02 21:42:47 +0300

    10.7
    CSV я произношу чсв

---
+++ 10/read_csv_dict.py
@@ -0,0 +1,17 @@
+import csv
+file = 'amilekoko.csv'
+def csv_reader(file):
+    people = []
+    try:
+        with open(file, 'r') as csv_file:
+            read = csv.reader(csv_file) #открытие файла
+            header = csv_file.readline().strip().split(',') #присвоение первой строчки файла header'у
+            eline = dict.fromkeys(header) #сделать шаблонный словарь с ключами header и значениями None
+            for line in read:
+                for i in eline.keys():
+                    eline[i] = line[list(eline.keys()).index(i)] #Изменить значения в шаблонном словаре на нужные
+                people.append(eline) 
+            return people
+    except Exception as e:
+        print(f'Возникла ошибка: {e}')
+print(csv_reader(file))
