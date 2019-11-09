5dce5cc67c2a722a7e8a8653892175b121580bed
kr33t9k <Kr33t9k@users.noreply.github.com>
2019-11-09 21:11:00 +0300

    Убрал костыли, исправил ошибку
    Убрал костыли, исправил ошибку

--- 10/my_tasks_json.py
+++ 10/my_tasks_json.py
@@ -7,40 +7,36 @@ text = '''Простой todo:
 
 filename = 'tasks.json'
 
-new_tasks = []
+def read(filename):
+    with open(filename, 'r') as fread:
+        global load 
+        load = json.load(fread)
 
-def one2(): #Если json файл уже существует в директории
-    new_task = {'Задача:': input('Сформулируйте задачу: '), 'Категория:': input('Выберите категорию: '), 'Время:': input('Добавьте время: ')}
-    new_tasks.append(new_task)
+if os.path.isfile(filename):
+    read(filename)
+else:
+    load = []
 
-def one1(): #Создание json файла и запись первой задачи в него. В конце даем указание использовать функцию выше
-    tasks = []
+def one():
     new_task = {'Задача:': input('Сформулируйте задачу: '), 'Категория:': input('Выберите категорию: '), 'Время:': input('Добавьте время: ')}
-    tasks.append(new_task)
-    with open(filename, 'w') as jz_file:
-        json.dump(tasks, jz_file)
-    do.update({1: one2})
+    print('\n')
+    load.append(new_task)
 
 def two():
-    with open(filename, 'r') as jo_file:
-        global load 
-        load = json.load(jo_file) #Достать список задач из json файла
-        if len(new_tasks) != 0: #Если есть новые задачи, добавить их в список выше
-            load += new_tasks
+    if load:
         for element in load:
             for key, value in element.items():
                 print(key, value, end = ', ')
             print('\n')
+    else:
+        print('Вы еще не добавили ни одной задачи.\n')
 
 def three():
-    with open(filename, 'w') as jz_file: #Запись новых задач при выходе, а не после каждого добавления
+    with open(filename, 'w') as jz_file:
         json.dump(load, jz_file)
     print('Данные записаны в файл')
 
-if os.path.isfile(filename): #Выбор первой функции в зависимости от наличия json файла в директории
-    do = {1: one2, 2: two, 3: three}
-else:
-    do = {1: one1, 2: two, 3: three}
+do = {1: one, 2: two, 3: three}
 
 while True:
     try:
@@ -49,7 +45,5 @@ while True:
         do[b]()
         if b == 3:
             break
-    except FileNotFoundError:
-        print('Вы еще не добавили ни одной задачи')
     except Exception as e:
         print(f'Произошла ошибка: {e}')
